868ba3c1ddf2eccd028c30d6b28a1986239a5e83
hkmangla <hemantmangla78@gmail.com>
2019-05-12 10:13:26 +0530

    implement build system using cmake

---
+++ libs/math/operations.hpp
@@ -0,0 +1,13 @@
+#ifndef OPERATIONS_HPP
+#define OPERATIONS_HPP
+
+namespace math {
+    class operations{
+    public:
+        int sum(const int &a, const int &b);
+        int mult(const int &a, const int &b);
+        int div(const int &a, const int &b);
+        int sub(const int &a, const int &b);
+    };
+}
+#endif
